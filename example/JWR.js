f186e35730d3d126340c42ce087e5ee93b7dd106
ZZY_WISU <79049368+EZ118@users.noreply.github.com>
2022-05-10 13:47:33 +0800

    Fixed A Few Bugs, Supports IE
    Fixed A Few Bugs, Supports IE
    Important Update

---
+++ example/JWR.js
@@ -0,0 +1,53 @@
+var LastURL = "";
+var UrlList = [{"subUrl":"default","realUrl":"./JWR.js"}];
+var Ele;
+
+function SetUrlList(json){UrlList = json;}
+
+function SetPage(ele){Ele = ele;}
+
+function GetPageUrl(subUrl){
+	subUrl = subUrl.split("/");
+	for(let i = 0; i < UrlList.length; i ++) {
+		let cnt = 0;
+		let slen = subUrl.length;
+		
+		if(subUrl[slen - 1] == "") {slen -= 1;}
+		
+		for(let j = 0; j < subUrl.length; j ++) {
+			if(UrlList[i]["subUrl"].split("/")[j] == subUrl[j]) {cnt ++;}
+		}
+		if (cnt == subUrl.length) {return UrlList[i]["realUrl"];}
+	}
+}
+
+function ShowPage(url) {
+	if (window.XMLHttpRequest) {xhttp = new XMLHttpRequest();}
+	else {xhttp = new ActiveXObject("Microsoft.XMLHTTP");}
+	
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			Ele.innerHTML = this.responseText;
+		}
+	};
+	xhttp.open("GET", url, true);
+	xhttp.send(); 
+}
+
+function GetData(url){
+	url += "#default";
+	url = url.split("#")[1];
+	url = GetPageUrl(url);
+	return url;
+}
+
+document.onmousedown = function(){
+	setTimeout(function(){
+		if(LastURL != window.location.href) {
+			rurl = GetData(window.location.href);
+			ShowPage(rurl);
+			//alert(GetPageUrl(GetData()));
+			LastURL = window.location.href;
+		}
+	}, 300);
+}
