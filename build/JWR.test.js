e6c1e1add62c82a409317f9808ff01397f629daf
ZZY_WISU <79049368+EZ118@users.noreply.github.com>
2022-06-29 09:03:13 +0800

    更新JWR.min.js

--- build/JWR.test.js
+++ build/JWR.test.js
@@ -5,6 +5,10 @@ var UrlList = ULJson = [
 		"realUrl":"./mainPage.htm"
 	},
 	{
+		"subUrl":"error",
+		"realUrl":"./error.htm"
+	},
+	{
 		"subUrl":"/home/",
 		"realUrl":"./mainPage.htm"
 	},
@@ -23,6 +27,7 @@ var LoaderStyle = {
 	"static":"position:fixed;top:0;left:0;background:#0066FF;height:3px;z-index:10;",
 	"active":"width"//此处可以填width或者height
 };
+var LoaderDelayTime = "0.25";
 
 window.onload = function(){
 	document.body.appendChild(LoaderEle);
@@ -56,8 +61,10 @@ function SetLoaderStyle(ls){
 	}
 }
 
-function GetPageUrl(subUrl){
+function GetPageUrl(subUrl, errorCode){
 	//从Json中通过假Url获取真实Url
+	if(errorCode == null) {errorCode = 0;}
+	
 	subUrl = subUrl.split("/");
 	for(let i = 0; i < UrlList.length; i ++) {
 		let cnt = 0;
@@ -70,6 +77,9 @@ function GetPageUrl(subUrl){
 		}
 		if (cnt == subUrl.length) {return UrlList[i]["realUrl"];}
 	}
+	if(errorCode == 0) {
+		return GetPageUrl("error", 1);
+	} else { return "error"; }
 }
 
 function ShowPage(url) {
@@ -82,7 +92,7 @@ function ShowPage(url) {
 		if (this.readyState == 4 && this.status == 200) {
 			Ele.innerHTML = this.responseText;
 			
-			LoaderEle.setAttribute("style", LoaderStyle["static"] + ";animation:jwr_anim .3s linear; animation-fill-mode:forwards;");
+			LoaderEle.setAttribute("style", LoaderStyle["static"] + ";animation:jwr_anim " + LoaderDelayTime + "s linear; animation-fill-mode:forwards;");
 			setTimeout(function() {
 				LoaderEle.setAttribute("style", "display:none;");
 			}, 400);
@@ -95,7 +105,7 @@ function ShowPage(url) {
 function GetData(url){
 	url += "#default";
 	url = url.split("#")[1];
-	url = GetPageUrl(url);
+	url = GetPageUrl(url, 0);
 	return url;
 }
 
