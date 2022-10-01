9e514dfcb8bbbf5b47cc378d7f465d2b1ad9af92
ZZY_WISU <79049368+EZ118@users.noreply.github.com>
2022-10-01 13:00:47 +0800

    Add files via upload

---
+++ example/setup.js
@@ -0,0 +1,26 @@
+ULJson = [
+	{
+		"subUrl":"default",
+		"realUrl":"./pages/mainPage.htm"
+	},
+	{
+		"subUrl":"error",
+		"realUrl":"./pages/error.htm"
+	},
+	{
+		"subUrl":"/home/",
+		"realUrl":"./pages/mainPage.htm"
+	},
+	{
+		"subUrl":"/test1/",
+		"realUrl":"./pages/subPage1.htm"
+	},
+	{
+		"subUrl":"/test1/ViewDevDoc/",
+		"realUrl":"./pages/subPage3.htm"
+	},
+	{
+		"subUrl":"/test2/",
+		"realUrl":"./pages/subPage2.htm"
+	}
+];
